7c9ab8199876deebc8f25dff0f1850be0ee8b877
WFrame0224 <wf0224@outlook.com>
2019-04-12 15:37:42 +0800

    增加了从起点到终点寻找最短路径的find方法，增强了程序的鲁棒性

--- 07_Dijkstra/DijkstraDemo.py
+++ 07_Dijkstra/DijkstraDemo.py
@@ -6,7 +6,7 @@
 @Author: Frame
 @LastEditors: Frame
 @Date: 2019-04-12 10:44:25
-@LastEditTime: 2019-04-12 15:07:09
+@LastEditTime: 2019-04-12 15:35:18
 ------------------------------------------------
 Dijkstra算法的步骤：
     1）找出起点权重值最低的节点，即消耗最小权重值可达的节点；
@@ -29,16 +29,17 @@ graph = {
 }
 # 记录开销的字典,记录从起点到达当前节点的最少开销
 costs = {
-    'A': 0,
-    'B': 3,
-    'C': 7,
+    'A': float('inf'),
+    'B': float('inf'),
+    'C': float('inf'),
     'D': float('inf'),
     'E': float('inf')
 }
 # 存储父节点的字典
 parents = {
-    'B': 'A',
-    'C': 'A',
+    'A': None,
+    'B': None,
+    'C': None,
     'D': None,
     'E': None
 }
@@ -77,9 +78,9 @@ def dijkstra(graph, costs, parents, processed):
     @param 
             graph : 有向图 
             costs : 开销表
-            parents : 父节点表,循环体结束后，这个加上起点，就是最终的路径
+            parents : 父节点表,循环体结束后就是最终的路径
             processed : 已经处理过的节点列表
-    @return: 
+    @return: None 算法实现中，parents存储的便是到某个节点的最短路径
     '''
     # 在未处理的节点中找出开销最少（路径权值最小）节点
     currentNode = findLowestNode(costs, processed)
@@ -100,13 +101,32 @@ def dijkstra(graph, costs, parents, processed):
         currentNode = findLowestNode(costs, processed)
 
 
-if __name__ == "__main__":
+def findShortestPath(graph,start,end):
+    '''
+    @name: findShortestPath()
+    @brief: 寻找从低点到终点的最短路径
+    @param {type} 
+    @return: 
+    '''
+    if start in graph.keys() and end in graph.keys():
+        # 起点从哪个节点开始，哪个节点的开销即为0
+        if start in costs.keys():
+            costs[start] = 0
+    else:
+        print('起点有误')
+        return
+    # 执行dijkstra算法，找出最短路径
     dijkstra(graph, costs, parents, processed)
     shortestPath = []
-    decNode = 'E'
+    decNode = end
     while decNode:
         shortestPath.append(decNode)
         decNode = parents.get(decNode)
     shortestPath.reverse()
-    print(shortestPath)
+
+    return shortestPath
+
+if __name__ == "__main__":
+    
+    print(findShortestPath(graph,'A','E'))
     
